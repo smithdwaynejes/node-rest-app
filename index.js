d568efd661e5d6c2d96e7c2e6aacf6b1eff4ff73
Heartly Regis <heartly.regis@codification.io>
2023-12-08 05:28:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/verify-email', (req, res) => {
+  const { email } = req.body;
+
+  if (email === 'my-wrong-email@example.com') {
+    const errorResponse = {
+      version: "1.0",
+      status: 409,
+      code: "errorCode",
+      requestId: "requestId",
+      userMessage: "The email you provided is invalid. Please try again.",
+      developerMessage: "The email you provided is invalid. Please try again.",
+      moreInfo: "https://docs.microsoft.com/en-us/azure/active-directory-b2c/string-transformations"
+    };
+
+    res.status(409).json(errorResponse);
+  } else {
+    res.status(200).json({ success: true, message: 'Email verification successful.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
